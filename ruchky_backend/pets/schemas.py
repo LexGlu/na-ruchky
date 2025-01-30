a00cf911912e5e56daa2a6bf570af09211b0c2ef
Oleksandr <alex.glushcenko@gmail.com>
2025-01-30 13:07:21 +0200

    Pet model upd for listing page

--- ruchky_backend/pets/schemas.py
+++ ruchky_backend/pets/schemas.py
@@ -1,12 +1,28 @@
+from typing import List
+
 from ninja import ModelSchema
 
-from ruchky_backend.pets.models import Pet, PetListing
+from ruchky_backend.pets.models import Pet, PetListing, PetSocialLink
+
+
+class PetSocialLinkSchema(ModelSchema):
+    class Meta:
+        model = PetSocialLink
+        fields = ["platform", "url"]
 
 
 class PetSchema(ModelSchema):
+    social_links: List[PetSocialLinkSchema]
+    tags: List[str] = []
+
     class Meta:
         model = Pet
         fields = "__all__"
+        exclude = ["tags", "tagged_items"]
+
+    @staticmethod
+    def resolve_tags(obj: Pet) -> List[str]:
+        return obj.tags.names()
 
 
 class PetListingSchema(ModelSchema):
