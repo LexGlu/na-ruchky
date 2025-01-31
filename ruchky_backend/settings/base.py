c58f94f322af59c05e45111de094aaf207b152d8
Oleksandr <alex.glushcenko@gmail.com>
2025-01-31 17:53:31 +0200

    Adds GZipMiddleware for HTTP responses compression

--- ruchky_backend/settings/base.py
+++ ruchky_backend/settings/base.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
